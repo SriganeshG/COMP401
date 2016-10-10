b11d50afa0836d38b09584719363c9da01d66227
Sriganesh Gopal <sriganesh.iswar@gmail.com>
2016-10-10 14:37:57 -0400

    Restarted A6Novice, need to clean up my code with inheritance and AnyPicture class.

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/84/5024391d188f00161c5c987301bc5ae8
@@ -0,0 +1,7 @@
+package a6novice;
+
+public interface SubPicture extends Picture {
+	Picture getSource();
+	int getXOffset();
+	int getYOffset();
+}
---
+++ Assignment_5/src/a5/srig/A6NoviceTest.java
@@ -0,0 +1,31 @@
+package a5.srig;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
+import org.junit.Test;
+
+import a6novice.*;
+
+public class A6NoviceTest {
+		
+	static public String[] getTestNames() {
+		String[] test_names = new String[2];
+		
+		test_names[0] = "exampleTest";
+		test_names[1] = "testOne";
+		
+		return test_names;
+	}
+		
+	@Test
+	public void exampleTest() {
+		
+	}
+	
+	@Test
+	public void testOne(){
+		
+	}
+}
